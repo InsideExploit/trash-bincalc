23c76dd6a1e09c9b9987ada47ec372e8c415bf91
InsideExploit <71732299+InsideExploit@users.noreply.github.com>
2020-10-17 20:48:56 +0200

    Included project files and required headers.

---
+++ BinaryCalculator/includes.h
@@ -0,0 +1,12 @@
+#include <Windows.h>
+#include <iostream>
+using namespace std;
+
+static int arrayBinary[256];
+static int arrayValue = 0;
+static int selectedOption = 0;
+static int decimalValue = 0;
+static int decimalBase = 1;
+
+static int decimalWait = 0;
+static int binaryWait = 0;
